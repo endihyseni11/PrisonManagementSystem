65f92a948bf9d05c9cf26ee2ce362f5a55a18ed9
endihyseni11 <161118663+endihyseni11@users.noreply.github.com>
2024-06-11 19:24:47 +0200

    Add files via upload

---
+++ prison-frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="footer-text">
+          Developed by <span className="developer-name">Arbër Ejupi</span>, <span className="developer-name">Granit Lekaj</span> and <span className="developer-name">Blerona Maxhuni</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
